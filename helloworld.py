71d6d0a98d702ff616e2e5a79908c6800410ddaa
harfordt <tim.harford@wgc.school.nz>
2019-02-14 15:16:55 +1300

    Added first label and comments

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,14 @@
-import tkinter
-root = tkinter.Tk()
+import tkinter	     # imports the tkinter library
+root = tkinter.Tk() # creates a Tk object (a 'window')
 
-root.title("First app!!")
-root.geometry("800x600")
+root.title("Hello World!!!")	# sets the title of the window
+root.geometry("600x200")    	# sets the window's width and height
+
+
+#create a label
+my_label = tkinter.Label(root)   #creates a label and attaches it to the window
+my_label.config(text="Hello!", fg="green")  # options to configure the label
+my_label.grid()        # a special method for placing the widget on the window
 
 root.mainloop()
+
