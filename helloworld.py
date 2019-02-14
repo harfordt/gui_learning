de5c2d72ae5e03251aced7e2c1ce70b970deb040
harfordt <tim.harford@wgc.school.nz>
2019-02-15 10:01:28 +1300

    Configured colors and fonts

--- helloworld.py
+++ helloworld.py
@@ -1,23 +1,20 @@
-import tkinter	     # imports the tkinter library
-root = tkinter.Tk() # creates a Tk object (a 'window')
+import tkinter
 
-root.title("Hello World!!!")	# sets the title of the window
-root.geometry("600x200")    	# sets the window's width and height
+root = tkinter.Tk()
+root.title("Hello World!!!")
+root.geometry("600x600")
 
-
-#create a label
 my_label = tkinter.Label(root)
-my_label.config(text="Hello!", fg="green")
+my_label.config(text="Hello!", fg="green", bg="yellow", font=("Wide Latin", "50", "italic"))
 my_label.grid()
 
-#create a label
 my_label2 = tkinter.Label(root)
-my_label2.config(text="Hey there!", fg="blue")
+my_label2.config(text="Hey there!", fg="blue", bg="white", font=("Stencil", "50"), padx="20")
 my_label2.grid()
 
-#create a label
 my_label3 = tkinter.Label(root)
-my_label3.config(text="Hey you!", fg="pink")
-my_label3.grid()  
+my_label3.config(text="Hey you!", fg="pink", bg="blue", font=("Times", "50", "bold"), pady="30")
+my_label3.grid()
+
 root.mainloop()
 
