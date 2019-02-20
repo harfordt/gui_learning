d13801f61d43ab420a506ac771ac813cc89d8ec7
harfordt <tim.harford@wgc.school.nz>
2019-02-20 14:09:43 +1300

    Added an extra button, created a setfunctoin for the label

--- three_buttonz.py
+++ three_buttonz.py
@@ -3,32 +3,28 @@ from tkinter import *
 root = Tk()
 
 
-def hello():
-    labeltext.set("Hello!")
-
-
-def banana():
-    labeltext.set("Banana :/")
-
-
-def clear():
-    labeltext.set("")
+def set_label1(text):
+    labeltext.set(text)
 
 
 labeltext = StringVar()
 labeltext.set("")
 
 button1 = Button(root)
-button1.config(text="Say hello!", command=hello)
+button1.config(text="Say hello!", command=lambda: set_label1("hello"))
 button1.grid()
 
 button2 = Button(root)
-button2.config(text="Say banana!", command=banana)
+button2.config(text="Say banana!", command=lambda: set_label1("banana ಠ_ಠ"))
 button2.grid()
 
 button3 = Button(root)
-button3.config(text="Clear text", command=clear)
+button3.config(text="Clear text", command=lambda: set_label1(""))
 button3.grid()
+
+button4 = Button(root)
+button4.config(text="I'm hungry", command=lambda: set_label1("I'm hungry"))
+button4.grid()
 
 label1 = Label(root)
 label1.config(textvariable=labeltext)
