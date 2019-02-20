6710235b89e11c99485f6b23a7c0db1233303b7c
harfordt <tim.harford@wgc.school.nz>
2019-02-20 14:31:30 +1300

    Added functions

--- calculator.py
+++ calculator.py
@@ -3,4 +3,26 @@ from tkinter import *
 root = Tk()
 root.title("Mr Harford's Calculator")
 
+
+def add_to_input_text(next_value):
+    current_text = input_text.get()
+    input_text.set(current_text + next_value)
+
+
+def calculate_answer():
+    ans = eval(input_text.get())
+    answer_value.set(ans)
+
+
+def clear():
+    input_text.set("")
+    answer_value.set("")
+
+
+input_text = StringVar()
+input_text.set("")
+
+answer_value = StringVar()
+answer_value.set("")
+
 root.mainloop()
