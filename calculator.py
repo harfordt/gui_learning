b3f93d53a0913f6a4634e601ce597440d92500ba
harfordt <tim.harford@wgc.school.nz>
2019-03-05 10:03:23 +1300

    Added new buttons and backspace functionality

--- calculator.py
+++ calculator.py
@@ -20,33 +20,66 @@ def clear():
     answer_value.set("")
 
 
+
+
+
+
+
+def backspace():
+    current_text = input_text.get()
+    new_text = current_text[0:len(current_text) - 1]
+    input_text.set(new_text)
+
+
+
+
+
+
+
 input_text = StringVar()
 input_text.set("")
 
 answer_value = StringVar()
 answer_value.set("")
 
-button1 = Button(root, text="7", width=3, command=lambda: add_to_input_text("7")).grid(row=0, column=0, sticky=N+E+S+W)
-button1 = Button(root, text="8", width=3, command=lambda: add_to_input_text("8")).grid(row=0, column=1, sticky=N+E+S+W)
-button1 = Button(root, text="9", width=3, command=lambda: add_to_input_text("9")).grid(row=0, column=2, sticky=N+E+S+W)
-button1 = Button(root, text="4", width=3, command=lambda: add_to_input_text("4")).grid(row=1, column=0, sticky=N+E+S+W)
-button1 = Button(root, text="5", command=lambda: add_to_input_text("5")).grid(row=1, column=1, sticky=N+E+S+W)
-button1 = Button(root, text="6", command=lambda: add_to_input_text("6")).grid(row=1, column=2, sticky=N+E+S+W)
-button1 = Button(root, text="1", width=3, command=lambda: add_to_input_text("1")).grid(row=2, column=0, sticky=N+E+S+W)
-button1 = Button(root, text="2", command=lambda: add_to_input_text("2")).grid(row=2, column=1, sticky=N+E+S+W)
-button1 = Button(root, text="3", command=lambda: add_to_input_text("3")).grid(row=2, column=2, sticky=N+E+S+W)
-button1 = Button(root, text="0", command=lambda: add_to_input_text("0")).grid(row=3, column=0, sticky=N+E+S+W)
+button1 = Button(root, text="7", width=3, command=lambda: add_to_input_text("7")).grid(row=0, column=0,
+                                                                                       sticky=N + E + S + W)
+button1 = Button(root, text="8", width=3, command=lambda: add_to_input_text("8")).grid(row=0, column=1,
+                                                                                       sticky=N + E + S + W)
+button1 = Button(root, text="9", width=3, command=lambda: add_to_input_text("9")).grid(row=0, column=2,
+                                                                                       sticky=N + E + S + W)
+button1 = Button(root, text="4", width=3, command=lambda: add_to_input_text("4")).grid(row=1, column=0,
+                                                                                       sticky=N + E + S + W)
+button1 = Button(root, text="5", command=lambda: add_to_input_text("5")).grid(row=1, column=1, sticky=N + E + S + W)
+button1 = Button(root, text="6", command=lambda: add_to_input_text("6")).grid(row=1, column=2, sticky=N + E + S + W)
+button1 = Button(root, text="1", width=3, command=lambda: add_to_input_text("1")).grid(row=2, column=0,
+                                                                                       sticky=N + E + S + W)
+button1 = Button(root, text="2", command=lambda: add_to_input_text("2")).grid(row=2, column=1, sticky=N + E + S + W)
+button1 = Button(root, text="3", command=lambda: add_to_input_text("3")).grid(row=2, column=2, sticky=N + E + S + W)
+button1 = Button(root, text="0", command=lambda: add_to_input_text("0")).grid(row=3, column=0, sticky=N + E + S + W)
 
-button1 = Button(root, text="+", width=3, command=lambda: add_to_input_text("+")).grid(row=0, column=3, sticky=N+E+S+W)
-button1 = Button(root, text="-", command=lambda: add_to_input_text("-")).grid(row=1, column=3, sticky=N+E+S+W)
-button1 = Button(root, text="*", command=lambda: add_to_input_text("*")).grid(row=2, column=3, sticky=N+E+S+W)
-button1 = Button(root, text="/", command=lambda: add_to_input_text("/")).grid(row=3, column=3, sticky=N+E+S+W)
+button1 = Button(root, text="+", width=3, command=lambda: add_to_input_text("+")).grid(row=0, column=3,
+                                                                                       sticky=N + E + S + W)
+button1 = Button(root, text="-", command=lambda: add_to_input_text("-")).grid(row=1, column=3, sticky=N + E + S + W)
+button1 = Button(root, text="*", command=lambda: add_to_input_text("*")).grid(row=2, column=3, sticky=N + E + S + W)
+button1 = Button(root, text="/", command=lambda: add_to_input_text("/")).grid(row=3, column=3, sticky=N + E + S + W)
 
-button1 = Button(root, text="=", command=lambda: calculate_answer()).grid(row=3, column=1, columnspan=2, sticky=N+E+S+W)
+button1 = Button(root, text="(", width=3, command=lambda: add_to_input_text("(")).grid(row=0, column=4,
+                                                                                       sticky=N + E + S + W)
+button1 = Button(root, text=")", width=3, command=lambda: add_to_input_text(")")).grid(row=1, column=4,
+                                                                                       sticky=N + E + S + W)
+button1 = Button(root, text=".", width=3, command=lambda: add_to_input_text(".")).grid(row=2, column=4,
+                                                                                       sticky=N + E + S + W)
+button1 = Button(root, text="Bksp", width=3, command=lambda: backspace()).grid(row=3, column=4, sticky=N + E + S + W)
+button1 = Button(root, text="=", command=lambda: calculate_answer()).grid(row=3, column=1, columnspan=2,
+                                                                          sticky=N + E + S + W)
 
-input_area = Label(root, textvariable=input_text, bg="hotpink", fg="white").grid(row=4, column=0, columnspan=4, sticky=N+E+S+W)
-answer_area = Label(root, textvariable=answer_value, bg="dodgerblue", fg="white").grid(row=5, column=0, columnspan=4, sticky=N+E+S+W)
+input_area = Label(root, textvariable=input_text, bg="hotpink", fg="white").grid(row=4, column=0, columnspan=5,
+                                                                                 sticky=N + E + S + W)
+answer_area = Label(root, textvariable=answer_value, bg="dodgerblue", fg="white").grid(row=5, column=0, columnspan=5,
+                                                                                       sticky=N + E + S + W)
 
-clear_button = Button(root, text="Clear", command=lambda: clear()).grid(row=6, column=0, columnspan=4, sticky=N+E+S+W)
+clear_button = Button(root, text="Clear", command=lambda: clear()).grid(row=6, column=0, columnspan=5,
+                                                                        sticky=N + E + S + W)
 
 root.mainloop()
