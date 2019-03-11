668d4a5f7eb90d1c6c3a6b46cfb287045135962e
harfordt <tim.harford@wgc.school.nz>
2019-03-12 10:34:12 +1300

    Added comments throughout

--- note_taker.py
+++ note_taker.py
@@ -1,56 +1,84 @@
 class Note:
+    """
+
+    """
+
     def __init__(self, title, text, category):
         self.__title = title
         self.__text = text
         self.__category = category
 
     def get_title(self):
+        """Getter function: gets the note title
+
+        Returns: the title of the note
+
+        """
         return self.__title
 
     def get_text(self):
+        """Getter function: gets the note body text
+
+        Returns: the body text of the note
+
+        """
         return self.__text
 
     def get_category(self):
-        return self.__category
+        """Getter function: gets the note category
 
+        Returns: the category of the note
+
+        """
+        return self.__category
 
 
 from tkinter import *
 
-
+# creates and sets up the window
 root = Tk()
 root.title("Lydia's Note Taker")
 root.option_add("*font", "Consolas 20")
 root.geometry("400x800")
 
+# this list is where all note objects will be stored
 notes = []
 
 
 def save_note(window, title, body, category="Shopping"):
-    print("Save note")
-    print(title)
-    print(body.strip())
-    print(category)
+    """Saves a note to the note list then closes the parent window.
 
+    Args:
+        window: the window this function was called from
+        title: the title of the note to be saved
+        body: the body text of the note to be saved
+        category: the category of the note to be saved
+    """
+
+    # creates the note object, after cleaning up the input values
     new_note = Note(title.title().strip(),
                     body.title().strip(),
                     category.title().strip())
 
+    # add the note to the note list
     notes.append(new_note)
-    print("Title: {}".format(new_note.get_title()))
-    print("Body: {}".format(new_note.get_text()))
-    print("Category: {}".format(new_note.get_category()))
+    # print("Title: {}".format(new_note.get_title()))
+    # print("Body: {}".format(new_note.get_text()))
+    # print("Category: {}".format(new_note.get_category()))
+
+    # close the window this function was called from
     window.destroy()
 
 
-
 def open_new_note():
+    """Creates a window for the user to enter their note in."""
     print("Open new note window")
     title_value = StringVar()
-    text_value = StringVar()
 
+    # creates the window
     new_note_window = Toplevel(root)
 
+    # creates and adds labels and text entry areas
     new_note_title = Label(new_note_window, text="New Note")
     new_note_title.grid()
 
@@ -67,6 +95,8 @@ def open_new_note():
     note_text.config(height=10, width=20)
     note_text.grid()
 
+    # a frame allows us to group GUI elements together
+    # this one will hold the buttons
     button_frame = Frame(new_note_window)
     button_frame.grid()
 
@@ -82,22 +112,36 @@ def open_new_note():
     save_button.grid(row=0, column=2, sticky=E)
 
 
-def open_list(list_name):
-    print("Open {}".format(list_name))
-    list_window = Toplevel(root)
-    list_window.title(list_name)
+def open_list(list_category):
+    """Opens a window with all of the notes of the given category.
 
+    Args:
+        list_category: the category this window should display
+    """
+    print("Open {}".format(list_category))
+
+    # creates and formats the window
+    list_window = Toplevel(root)
+    list_window.title(list_category)
+    list_window.geometry("600x400")
+
+    # takes each note from the list, formats it and adds it to the window
     for note in notes:
-        title =note.get_title()
+        title = note.get_title()
         body = note.get_text()
         category = note.get_category()
 
-        note_text ="***{}***\n{}\n".format(title,body)
+        note_text = "***{}***\n{}\n".format(title, body)
 
-        Label(list_window,text=note_text).grid(sticky=W)
+        # only add notes with the relevant category
+        if category == list_category:
+            Label(list_window, text=note_text).grid(sticky=W)
 
 
-
+"""
+    The main (root) window is configured from here down.
+    It is not in a function so all widgets etc are globally accessible.
+"""
 lbl_title = Label(root, text="Notes")
 lbl_title.config(font=("Calibri", "30"), width=20)
 lbl_title.grid(row=0, sticky=N + E + S + W)
@@ -118,4 +162,5 @@ btn_homework = Button(root, text="Homework", command=lambda: open_list("Homework
 btn_homework.config()
 btn_homework.grid(row=4, sticky=E + W)
 
+# this needs to be the last line - anything after this won't be seen
 root.mainloop()
