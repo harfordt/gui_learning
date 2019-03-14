26a3838063a7e25db077e385ba6815985f6a4fbd
harfordt <tim.harford@wgc.school.nz>
2019-03-14 15:13:32 +1300

    Remove add not button, add '+' buttons next to categories

--- note_taker.py
+++ note_taker.py
@@ -143,24 +143,34 @@ def open_list(list_category):
     It is not in a function so all widgets etc are globally accessible.
 """
 lbl_title = Label(root, text="Notes")
-lbl_title.config(font=("Calibri", "30"), width=20)
+lbl_title.config(font=("Calibri", "30"), width=18)
 lbl_title.grid(row=0, sticky=N + E + S + W)
 
-btn_newnote = Button(root, text="+ New note", command=lambda: open_new_note())
-btn_newnote.config()
-btn_newnote.grid(row=1, sticky=W)
+
 
 btn_shopping = Button(root, text="Shopping", command=lambda: open_list("Shopping"))
 btn_shopping.config()
-btn_shopping.grid(row=2, sticky=E + W)
+btn_shopping.grid(column=0, row=1, sticky=E + W)
+
+btn_shopping_new = Button(root, text="+", command=lambda: open_new_note("Shopping"))
+btn_shopping_new.config()
+btn_shopping_new.grid(column=1, row=1, sticky=E + W)
 
 btn_todo = Button(root, text="To do", command=lambda: open_list("To do"))
 btn_todo.config()
-btn_todo.grid(row=3, sticky=E + W)
+btn_todo.grid(column=0, row=2, sticky=E + W)
+
+btn_todo_new = Button(root, text="+", command=lambda: open_new_note("To do"))
+btn_todo_new.config()
+btn_todo_new.grid(column=1, row=2, sticky=E + W)
 
 btn_homework = Button(root, text="Homework", command=lambda: open_list("Homework"))
 btn_homework.config()
-btn_homework.grid(row=4, sticky=E + W)
+btn_homework.grid(column=0, row=3, sticky=E + W)
+
+btn_homework_new = Button(root, text="+", command=lambda: open_new_note("Homework"))
+btn_homework_new.config()
+btn_homework_new.grid(column=1, row=3, sticky=E + W)
 
 # this needs to be the last line - anything after this won't be seen
 root.mainloop()
